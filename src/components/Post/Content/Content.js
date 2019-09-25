411307aece0ddbd43128ca868b71109c9507ce52
Pike96 <linyucheng256@gmail.com>
2019-09-24 18:46:15 -0700

    Adjust post style

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,17 +1,26 @@
 // @flow strict
 import React from 'react';
 import styles from './Content.module.scss';
+import Meta from '../Meta';
+import Tags from '../Tags';
 
 type Props = {
   body: string,
   title: string
 };
 
-const Content = ({ body, title }: Props) => (
-  <div className={styles['content']}>
-    <h1 className={styles['content__title']}>{title}</h1>
-    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
-  </div>
-);
+const Content = ({ body, title, fields, frontmatter }: Props) => {
+  const { tagSlugs } = fields;
+  const { tags, date } = frontmatter;
+
+  return (
+    <div className={styles['content']}>
+      <h1 className={styles['content__title']}>{title}</h1>
+      {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
+      <Meta date={date} />
+      <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+    </div>
+  );
+};
 
 export default Content;
